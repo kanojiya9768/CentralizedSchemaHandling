f67c2903d1ecd954b1c521fa37bf3bfed3c7ee2b
kanojiyaBoppo9768 <vishal.kanojiya@boppotechnologies.com>
2024-08-08 21:33:05 +0530

    first commit

--- models/type1/model1.model.js
+++ models/type1/model1.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { toJSON, paginate } = require('./plugins');
+const { toJSON, paginate } = require('../plugins');
 
 const modelSchema = mongoose.Schema({
     // Your schema here
